0216397a5c5d0497d099a164ff26fb1b3e57b750
Tuicy <gwt523@163.com>
2019-03-29 17:55:50 +0800

    add .gitignore 忽略.class文件

--- InputTest.java
+++ InputTest.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.Console;
 
 /* This program demonstrates console input. */
 
@@ -18,5 +19,9 @@ public class InputTest
 
         // display output on console
         System.out.println("Hello, " + name + ". Next year,you'll be " + (age + 1));
+
+        Console cons = System.console();
+        String username = cons.readLine("User name: ");
+        char[] passwd = cons.readPassword("Password: "); // 输入密码，且输入时不可见
     }
 }
